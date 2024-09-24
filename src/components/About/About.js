70a1e01ce3c9ccb216eb0c1ebc0ae6f1cbbff0ed
Alexander Tolkachev <alexandr.tolkachyov@gmail.com>
2024-09-24 10:25:11 +0800

    learned how to render lists from APIs using pokeapi

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './About.css'
 
 export default function About() {
 
-    const [employees, setEmployees] = useState(['Bob Marley', 'Curt Cobain', 'Darth Vader']);
+    const [pokemons, setPokemons] = useState([]);
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+            .then((data) => data.json())
+            .then((res) => setPokemons(res.results))
+    }, [])
     return (
-        <div className="about-main-wrapper container">
+        <div className="about-main-wrapper">
             <h1 className='main-wrapper__head-title'>About our employees</h1>
-            <div>
-                {employees.map((el, i) => (
-                    <p key={i}>{el}</p>
+            <div className='pokemons-grid'>
+                {pokemons.map((el, i) => (
+                    <div className="pokemons-card" key={i}>
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`} />
+                        <h3>{el.name}</h3>
+                    </div>
                 ))}
             </div>
         </div>
